b347edf79016c6c72f1c63d7cca68e7f99ae97da
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-10-30 19:46:54 +0100

    Add mmml_version package

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 import setuptools  # type: ignore
 
-MAJOR, MINOR, PATCH = 0, 3, 3
-VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
-"""This project uses semantic versioning.
-See https://semver.org/
-Before MAJOR = 1, there is no promise for
-backwards compatibility between minor versions.
-"""
+version = {}
+with open("mutwo/mmml_version/__init__.py") as fp:
+    exec(fp.read(), version)
+
+VERSION = version["VERSION"]
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
