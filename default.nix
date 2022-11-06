1fd0988c02a3d9ee99dd473023c8c1d0cb081c45
levinericzimmermann <levinericzimmermann@posteo.eu>
2022-11-06 18:42:05 +0100

    Update nix

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 with import <nixpkgs> {};
-with pkgs.python3Packages;
+with pkgs.python310Packages;
 
 let
 
-  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/97aea97f996973955889630c437ceaea405ea0a7.tar.gz";
+  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/61ebb657ef5806eb067f5df6885254fdbae8f44c.tar.gz";
   mutwo-core = import (mutwo-core-archive + "/default.nix");
 
-  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/4e4369c1c9bb599f47ec65eb86f87e9179e17d97.tar.gz";
+  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/d90b6db7d433d64c25f039adcd6b41075c05c013.tar.gz";
   mutwo-music = import (mutwo-music-archive + "/default.nix");
 
 in
@@ -16,8 +16,8 @@ in
     src = fetchFromGitHub {
       owner = "mutwo-org";
       repo = name;
-      rev = "c2cc56052a97df7291c4a51b6c3ad31e45fcadcb";
-      sha256 = "sha256-cqVXtHE60bqdN7YHzIBswUY2pFMKPon/Yrpno5lSrWk=";
+      rev = "b57f0cd2c91a08f75e79312e90aa455347a3b8ad";
+      sha256 = "sha256-xVVC7EXp09n+5LR9CwreOgEWRGlJOc1rSkN4elp580Q=";
     };
     propagatedBuildInputs = [
       mutwo-core
